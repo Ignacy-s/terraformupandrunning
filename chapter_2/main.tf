8cf21418e9971458e17cc6c53e2f9773862d344e
Ignacy Sawa <bd1cb5c6@opayq.com>
2023-04-03 20:51:45 +0200

    Start Deploy Simple Web Server, add user_data

--- chapter_2/main.tf
+++ chapter_2/main.tf
@@ -2,13 +2,21 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "teruarc2-1" {
+resource "aws_instance" "teruarc2-2" {
   ami ="ami-064087b8d355e9051"
   instance_type = "t3.nano"
 
   tags = {
     Name = "terraform-is-up"
   }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello World!" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+  
+  user_data_replace_on_change = true
 
   credit_specification {
     cpu_credits = "standard"
