fb63264fcfb85382ba44ac6c688098c44eb9dd07
Ignacy Sawa <bd1cb5c6@opayq.com>
2023-04-05 14:35:32 +0200

    Add security group, test the web server with curl

--- chapter_2/main.tf
+++ chapter_2/main.tf
@@ -2,10 +2,21 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "teruarc2-2" {
   ami ="ami-064087b8d355e9051"
   instance_type = "t3.nano"
-
+  vpc_security_group_ids = [aws_security_group.instance.id]
   tags = {
     Name = "terraform-is-up"
   }
